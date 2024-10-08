581b0804fd38293b451055b7680be1891fde4fb9
Sabricetin <sabriyazilim@gmail.com>
2024-10-08 14:17:08 +0300

    Initial Commit

---
+++ ToDoSwfitData/ToDoSwfitDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwfitDataApp.swift
+//  ToDoSwfitData
+//
+//  Created by Sabri Çetin on 8.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwfitDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
